d0c5090fa11160fbb5ce15a3c7ecafadbf6b2546
shridharkulkarni-eaton <shridharskulkarni@eaton.com>
2023-10-26 14:20:57 +0530

    changes made for merge

--- conflict_check.py
+++ conflict_check.py
@@ -1,5 +1,10 @@
 ## this file is to check conflict
 a = input("Give me a number: ")
 b = input("Give me a number: ")
+a = int(a)
+b = int(b)
+
+a = input("Give me a number: ")
+b = input("Give me a number: ")
 a = float(a)
 b = int(b)
