a46e894d3d39de5a1d2f173a121025bdd9488f71
shridharkulkarni-eaton <shridharskulkarni@eaton.com>
2023-10-26 11:46:30 +0530

    Added two math fucntions to two different branches

---
+++ addtion.py
@@ -0,0 +1,11 @@
+##
+"""This fucntions take input a and b as float and add them"""
+
+
+def add(a, b):
+    """This fucntions take input a and b as float and add them"""
+    a = float(a)
+    b = float(b)
+    c = a + b
+    print("Addtion of {a} and {b} is {c}")
+    return c
